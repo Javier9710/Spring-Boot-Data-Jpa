e954a41e4c3b135af83eadbc5b5440ea6442609a
unknown <javierandresmd@ufps.edu.co>
2021-01-13 21:24:28 -0500

    Creando entidad con @Entity y Clase Dao

---
+++ src/main/java/com/ufps/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.ufps.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.ufps.springboot.app.models.entities.Cliente;
+
+public interface IClienteDao {
+	
+	public List<Cliente> findAll();
+
+}
