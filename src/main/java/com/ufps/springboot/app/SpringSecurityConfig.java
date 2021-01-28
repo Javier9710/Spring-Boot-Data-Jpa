cbb138f63944ac4fce62a3616d75f29c5d60bb4c
unknown <javierandresmd@ufps.edu.co>
2021-01-27 19:48:58 -0500

    Manejando error de acceso denegado creando la vista error_403

--- src/main/java/com/ufps/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/ufps/springboot/app/SpringSecurityConfig.java
@@ -29,7 +29,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.formLogin().loginPage("/login")
 		.permitAll()
 		.and()
-		.logout().permitAll();
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
 	}
 
 
