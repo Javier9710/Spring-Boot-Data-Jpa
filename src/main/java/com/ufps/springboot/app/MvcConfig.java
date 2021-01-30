f0e43a8860bdb4ae18bcb7129a19e5f417b260c1
unknown <javierandresmd@ufps.edu.co>
2021-01-29 20:35:47 -0500

    Configuracion para manejar Multilenguaje del sistema

--- src/main/java/com/ufps/springboot/app/MvcConfig.java
+++ src/main/java/com/ufps/springboot/app/MvcConfig.java
@@ -1,13 +1,17 @@
 package com.ufps.springboot.app;
 
 import java.nio.file.Paths;
+import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -30,6 +34,20 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(new Locale("es","ES"));
+		return localResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localInterceptor = new LocaleChangeInterceptor();
+		localInterceptor.setParamName("lang");
+		return localInterceptor;
 	}
 
 }
